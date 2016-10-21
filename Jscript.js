445e9495b9b070255a6e6b8016ff4bd9deb9d38c
Niephox <rezutasos@gmail.com>
2016-10-21 23:57:49 +0300

    Cleaning Branch up

--- Jscript.js
+++ Jscript.js
@@ -1,17 +1,34 @@
-	var x = document.getElementById("GeoLoc");
-
-	function getLocation() {
-		if (navigator.geolocation){
-			navigator.geolocation.getCurrentPosition(showPosition);
-		}	
-		else {	
-			x.innerHTML ="Geolocation is not supported by this Browser.";
-		}
-	}
-
-	function showPosition(position){
-		x.innerHTML ="Latitude: " + position.coords.latitude + "<br> Longitude: " + position.coords.longitude;		
+var x = document.getElementById("GeoLoc");
+		
+function getLocation() {
+    //Check if Navigator is supported    
+	if (navigator.geolocation){
+		navigator.geolocation.getCurrentPosition(showPosition,showError); // if so return coords obj to showPosition
 	}	
+	else {	
+		x.innerHTML ="Geolocation is not supported by this browser." + navigator.appName;
+	}
+}	
 
+function showPosition(position){
+	x.innerHTML ="Latitude: " + position.coords.latitude + "<br> Longitude: " + position.coords.longitude + "<br> ON " + navigator.appName;		
+}	
+//Error Handling on getting position
+function showError(error) {
+	switch(error.code) {
+		case error.PERMISSION_DENIED:
+	   		x.innerHTML = "Geolocation Denied by user." + "<br> ON " + navigator.appName;
+	    	break;
+	    case error.POSITION_UNAVAILABLE:
+	    	x.innerHTML = "Location Information is unavailable." + "<br> ON " + navigator.appName;
+	    	break;
+	    case error.TIMEOUT:
+	    	x.innerHTML = "The request timed out." + "<br> ON " + navigator.appName;
+	    	break;
+	    case error.UNKNOWN_ERROR:
+	    	x.innerHTML = "Unknown error" + "<br> ON " + navigator.appName;
+	    	break;
+	}
+}
 
 
